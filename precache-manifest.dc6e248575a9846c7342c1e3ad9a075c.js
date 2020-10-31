c1b5c625b3e57bd4a7d5b2863692ec3b70271dd9
Abdullah Alabbad <a.a.al3bad@outlook.com>
2020-10-31 22:19:04 +1100

    Updates

---
+++ precache-manifest.dc6e248575a9846c7342c1e3ad9a075c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8130f0f3650dda02397d06845f8816aa",
+    "url": "/dev-life-tracker/index.html"
+  },
+  {
+    "revision": "d3fc18094d02fd8bfbe0",
+    "url": "/dev-life-tracker/static/css/main.d1ed271e.chunk.css"
+  },
+  {
+    "revision": "be8bd3226fa8cf3f9c7b",
+    "url": "/dev-life-tracker/static/js/2.fd45a1df.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/dev-life-tracker/static/js/2.fd45a1df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3fc18094d02fd8bfbe0",
+    "url": "/dev-life-tracker/static/js/main.346c6ddb.chunk.js"
+  },
+  {
+    "revision": "c267c71a5c188522c967",
+    "url": "/dev-life-tracker/static/js/runtime-main.8f1b2416.js"
+  }
+]);
